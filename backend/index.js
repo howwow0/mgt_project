291f0d746503656422665fa54cf9ec225c3060db
Даниил Купцов <danilkup23@yandex.ru>
2024-09-27 21:34:08 +0400

    push algorithm

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+//прирост населения в зависимости от площади
+function PoppulationGrowth(square, category){
+    return square / category;
+}
+
+//прирост пассажиропотока (офисная и жилая область застройки)
+function PassangerGrowth(officePoppulationGrowth, livingPoppulationGrowth){
+    return officePoppulationGrowth + livingPoppulationGrowth * 0.57 * 0.8; 
+}
+
+//прирост пассажиропотока по типу: ИТ или ОТ
+function TypePassangerGrowth(passangerGrowth, type){
+    return type * passangerGrowth;
+}
+
+//сумма узлов пассажиропотока (и для ИТ, и для ОТ)
+function PassangerTraffic(nodeArray, countNode, transportFactor = 1){
+    let sum = 0;
+    for (let i = 0; i < countNode; i++){
+        sum += nodeArray[i];
+    }
+    return sum * transportFactor;
+}
+
+//новый пассажиропоток
+function NewPassangerTraffic(passangerTraffic, typePassangerGrowth){
+    return passangerTraffic + typePassangerGrowth;
+}
+
+//новый пассажиропоток на узле
+function NewNodePassangerTraffic(typePassangerGrowth, passangerTraffic, nodePassangerTraffic){
+    return typePassangerGrowth * nodePassangerTraffic / passangerTraffic;
+}
+
+//наличие дефицита
+function Delta(newPassangerTraffic, trafficCapacity){
+    if (newPassangerTraffic - trafficCapacity > 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+
+
+
+
