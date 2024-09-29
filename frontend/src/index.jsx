049b07ca2fc99e0e33db93899208c032801c118f
andrew-dot-exe <andrejnikitin974@gmail.com>
2024-09-29 23:52:40 +0400

    added vite to frontend

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
