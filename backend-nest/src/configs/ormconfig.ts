d0ba1f93abcd8f04f60872a7f26453ea85bd1a5c
HowWow <yarohsyk10@gmail.com>
2024-09-30 19:40:57 +0400

    Update readme

--- backend-nest/src/configs/ormconfig.ts
+++ backend-nest/src/configs/ormconfig.ts
@@ -3,13 +3,13 @@ import { DataSource } from 'typeorm';
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'sa',
+  port: 5432, 
+  username: 'postgres', //Ваш username
+  password: 'sa', //Ваш пароль
   logging: true,
   synchronize: false,
   migrationsRun: false,
-  database: 'construction_db',
+  database: 'construction_db', //Ваша БД, ВАЖНО! Ее необходимо самому создать в PostgreSQL
   entities: ['**/*.entity.{ts,js}'],
   migrations: ['src/migrations/*.ts'],
 });
